4bb399d80ac15eccb4d4ca2ffac5c17e7d5704bf
osimloeff <mloeff@osisoft.com>
2018-05-16 12:53:12 -0300

    Added tests

---
+++ src/OSIsoft.PIDevClub.PIWebApiClient/OSIsoft.PIDevClub.PIWebApiClient.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OSIsoft.PIDevClub.PIWebApiClient.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OSIsoft.PIDevClub.PIWebApiClient.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ba66b80-6e8d-45d5-9887-57c9aaa7ba12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
